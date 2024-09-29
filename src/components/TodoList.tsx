afa7f0ad2761862929c4806c2a76d3678bb8e940
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-09-29 21:54:19 +0900

    Todoリスト完成

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -17,16 +17,37 @@ const TodoList = () => {
   ]
 
   const [todos, setTodos] = useState(initialState);
+  const [task, setTask] = useState('')
+
+  const handleNewTask = (event) => {
+    setTask( event.target.value )
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if(task === '') return
+    setTodos(todos => [...todos,{ task, isCompleted: false}])
+    setTask('')
+  }
 
   return(
     <div>
       <h1 className="text-3xl font-bold underline">Todo List</h1>
+      <form onSubmit={handleSubmit} class="mt-4">
+        Add Task　:　
+        <input 
+          value={ task } 
+          placeholder="Add New Task" 
+          class="input input-bordered input-sm w-full max-w-xs" 
+          onChange={handleNewTask}
+        />
+        <button type="submit" class="btn btn-sm ml-2">Add</button>
+      </form>
       <ul>
         { todos.map((todo, index) => (
           <li key={ index }>{ todo.task }</li>
         ))}
       </ul>
-      <button className="btn btn-primary">Button</button>
     </div>
   );
 }
